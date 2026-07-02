360e9848a9ce4b12edd88ccf821b888c158ca27d
Jay Lander <jamallander@gmail.com>
2026-07-01 23:41:32 -0500

    Initial commit: Spring Boot 4.0.x project structure

---
+++ src/main/java/com/autobio/api/AutobioApiApplication.java
@@ -0,0 +1,13 @@
+package com.autobio.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutobioApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutobioApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/autobio/api/AutobioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autobio.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutobioApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
