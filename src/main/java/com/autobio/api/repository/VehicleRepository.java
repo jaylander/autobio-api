e4b42cdcc24ad47f40ba05aec8ca0435b77e182b
Jay Lander <jamallander@gmail.com>
2026-07-21 18:00:46 -0500

    Add UserRepository, VehicleRepository, application-test.properties.example Update .gitignore. Relates to #2

---
+++ src/main/java/com/autobio/api/repository/VehicleRepository.java
@@ -0,0 +1,15 @@
+package com.autobio.api.repository;
+
+import com.autobio.api.model.Vehicle;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+    List<Vehicle> findByUserId(Long userId);
+    Optional<Vehicle> findByIdAndUserId(Long id, Long userId);
+
+}
